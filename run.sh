46d050dbb43e0ad5f7256e33fb5c7d24b8361eb2
Scott Newman <snewman18@gmail.com>
2011-04-26 13:21:05 -0400

    Removing files that were moved to extras.

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export RUBYOPT=rubygems
-d=`dirname $0`
-ruby $d/backend_bot.rb start
-ruby $d/listener_bot.rb start
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export RUBYOPT=rubygems
-d=`dirname $0`
-ruby $d/backend_bot.rb stop
-ruby $d/listener_bot.rb stop
